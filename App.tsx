c33082e12fef717d8a1ded20a31c0df2a8f91596
RederAc3 <reder.ac3@gmail.com>
2022-11-14 21:25:45 +0100

    Change style StatusBar to dark

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from 'expo-status-bar';
+
 import NewsListScreen from "./src/screens/NewsListScreen/NewsListScreen";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
 
@@ -9,6 +11,7 @@ const auth = false
 const Root = () => {
   return (
     <NavigationContainer>
+      <StatusBar style="dark" />
       <Stack.Navigator>
         {
           !auth ? (
