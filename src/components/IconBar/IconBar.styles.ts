d9c1099b55bac313656d96bc4c44a926a9e8135e
RederAc3 <reder.ac3@gmail.com>
2022-11-14 22:59:36 +0100

    Reverse order of numbers and icon display in IconBar

--- src/components/IconBar/IconBar.styles.ts
+++ src/components/IconBar/IconBar.styles.ts
@@ -14,6 +14,6 @@ export default StyleSheet.create({
     
   },
   iconNumber: {
-    marginRight: 10
+    marginLeft: 5
   }
 });
--- src/components/IconBar/IconBar.tsx
+++ src/components/IconBar/IconBar.tsx
@@ -8,13 +8,13 @@ const IconBar: React.FC<IconBarProps> = ({ numberOfLikes, numberOfComments, isLi
     return (
         <View style={styles.iconBar}>
             <View style={styles.iconItem}>
-                <Text style={styles.iconNumber}>{numberOfComments}</Text>
                 <FontAwesome name="comment-o" size={24} color="black" />
+                <Text style={styles.iconNumber}>{numberOfComments}</Text>
             </View>
             <View style={styles.iconItem}>
-                <Text key={1} style={styles.iconNumber}>{numberOfLikes}</Text>
                 {isLiked ? (<FontAwesome name="thumbs-up" size={24} color="black" />
                 ) : <FontAwesome name="thumbs-o-up" size={24} color="black" />}
+                <Text key={1} style={styles.iconNumber}>{numberOfLikes}</Text>
             </View>
         </View>
     )
